142c7cf9ab39100f1c4a133fac6ce3c387f7bebd
N-Pex <npexdev@gmail.com>
2014-03-19 14:43:41 +0100

    Shoe "up" icon for About, Settings and Share App

--- app/src/info/guardianproject/securereaderinterface/SettingsActivity.java
+++ app/src/info/guardianproject/securereaderinterface/SettingsActivity.java
@@ -56,6 +56,7 @@ public class SettingsActivity extends FragmentActivityWithMenu
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setDisplayHomeAsUp(true);
 		setContentView(R.layout.activity_settings);
 		setMenuIdentifier(R.menu.activity_settings);
 
