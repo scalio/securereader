fa577d2fc92ad5482cd1928e2133287a21265195
N-Pex <npexdev@gmail.com>
2014-04-04 11:05:51 +0200

    Turned FocusNoHintEditText into general widget
    With comments and all, for simple reuse.

--- app/src/info/guardianproject/securereaderinterface/widgets/FocusNoHintEditText.java
+++ app/src/info/guardianproject/securereaderinterface/widgets/FocusNoHintEditText.java
@@ -1,20 +1,28 @@
 package info.guardianproject.securereaderinterface.widgets;
 
-import ch.boye.httpclientandroidlib.util.TextUtils;
 import android.content.Context;
 import android.graphics.Rect;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.TextUtils;
 import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.widget.EditText;
 
+/**
+ * This is a variation of the EditText class that will hide the hint text when it has
+ * focus (normal EditText will always show the hint if the input text is empty, even when
+ * is has focus).
+ * Also, the hint text size can be adjusted relative to the set text size of the widget
+ * by calling {@link #setHintRelativeSize(float)}.
+ *
+ */
 public class FocusNoHintEditText extends EditText
 {
-
 	private CharSequence mHint;
-
+	private float mHintRelativeSize = 1.0f;
+	
 	public FocusNoHintEditText(Context context)
 	{
 		super(context);
@@ -33,18 +41,44 @@ public class FocusNoHintEditText extends EditText
 		init();
 	}
 
+	/**
+	 * Get the hint size as a fraction of the normal text size. Default is 1.0 (hint text
+	 * is the same size as input text).
+	 * @return hint size as a fraction of the normal text size.
+	 * @see #setHintRelativeSize(float)
+	 */
+	public float getHintRelativeSize()
+	{
+		return mHintRelativeSize;
+	}
+
+	/**
+	 * Set the hint size as a fraction of the normal text size.
+	 * @param relativeSize hint size as a fraction of the normal text size.
+	 * @see #getHintRelativeSize()
+	 */
+	public void setHintRelativeSize(float relativeSize)
+	{
+		mHintRelativeSize = relativeSize;
+		
+		// If we have focus, the change will be picked up later, otherwise need to
+		// change it now.
+		if (!hasFocus())
+			setAndResizeHint();
+	}
+	
 	private void init()
 	{
 		mHint = getHint();
-		applyHintTextSmall();
+		setAndResizeHint();
 	}
 
-	private void applyHintTextSmall()
+	private void setAndResizeHint()
 	{
 		if (!TextUtils.isEmpty(mHint))
 		{
 			SpannableString span = new SpannableString(mHint);
-			span.setSpan(new RelativeSizeSpan(0.7f), 0, mHint.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+			span.setSpan(new RelativeSizeSpan(getHintRelativeSize()), 0, mHint.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 			super.setHint(span);
 		}
 	}
@@ -56,11 +90,11 @@ public class FocusNoHintEditText extends EditText
 		if (focused)
 			super.setHint("");
 		else
-			applyHintTextSmall();
+			setAndResizeHint();
 	}
 	
     @Override
-    public boolean onKeyPreIme(int keyCode, KeyEvent event)
+	public boolean onKeyPreIme(int keyCode, KeyEvent event)
     {
         if (event.getKeyCode() == KeyEvent.KEYCODE_BACK)
         {
