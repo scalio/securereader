813f83237efbdeaca1709fedb2944cd813f330f3
Jason Dartmouth <jxdartmouth@gmail.com>
2014-06-04 15:35:08 -0400

    Readding ActionBarActivity import

--- app/src/info/guardianproject/securereaderinterface/LockableActivity.java
+++ app/src/info/guardianproject/securereaderinterface/LockableActivity.java
@@ -14,6 +14,7 @@ import android.os.PowerManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
